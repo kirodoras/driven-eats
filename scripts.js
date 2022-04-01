5cfcbc98befd3a4a28a78b2373bf9c007cdb4d54
MateusFigueiredo <figueiredo1497@gmail.com>
2022-03-31 23:56:29 -0300

    selecao de cards aprimorado

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
-function cardSelect(whichCard) {
-    const cardSelecionado = document.querySelector(".Selecionado");
+function cardSelectPrato(whichCard) {
+    const cardSelecionado = document.querySelector(".pratoSelecionado");
 
     if(cardSelecionado !== null) {
-        cardSelecionado.classList.remove("Selecionado");
+        cardSelecionado.classList.remove("pratoSelecionado");
     }
 
-    whichCard.classList.add("Selecionado");
+    whichCard.classList.add("pratoSelecionado");
+}
+
+function cardSelectBebida(whichCard) {
+    const cardSelecionado = document.querySelector(".bebidaSelecionado");
+
+    if(cardSelecionado !== null) {
+        cardSelecionado.classList.remove("bebidaSelecionado");
+    }
+
+    whichCard.classList.add("bebidaSelecionado");
+}
+
+function cardSelectSobremesa(whichCard) {
+    const cardSelecionado = document.querySelector(".sobremesaSelecionado");
+
+    if(cardSelecionado !== null) {
+        cardSelecionado.classList.remove("sobremesaSelecionado");
+    }
+
+    whichCard.classList.add("sobremesaSelecionado");
 }
