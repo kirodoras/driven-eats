1e3cfdfb154f15f14e9abdcf5c7d855b84366d2d
MateusFigueiredo <figueiredo1497@gmail.com>
2022-04-01 02:49:06 -0300

    envio do pedido

--- scripts.js
+++ scripts.js
@@ -45,3 +45,25 @@ function checkThree () {
             buttonClose.innerHTML = "Fechar pedido";
     }
 }
+
+function sendRequest (){
+    const buttonStatus = document.querySelector(".buttonActive");
+
+    if(buttonStatus !== null) {
+        const infoPratos = document.querySelector(".pratoSelecionado h4").innerHTML;
+        let precoPratos = document.querySelector(".pratoSelecionado strong").innerHTML.replace("," , ".");
+
+        const infoBebida = document.querySelector(".bebidaSelecionado h4").innerHTML;
+        let precoBebida = document.querySelector(".bebidaSelecionado strong").innerHTML.replace("," , ".");
+
+        const infoSobremesa = document.querySelector(".sobremesaSelecionado h4").innerHTML;
+        let precoSobremesa = document.querySelector(".sobremesaSelecionado strong").innerHTML.replace("," , ".");
+
+        let total = Number(precoPratos) + Number(precoBebida) + Number(precoSobremesa );
+
+        let envioMensagem = "Olá, gostaria de fazer o pedido:\n"+"- Prato: " + infoPratos + "\n- Bebida: " + infoBebida + "\n- Sobremesa: " + infoSobremesa + "\nTotal: R$ " + total.toFixed(2);
+        envioMensagem = encodeURIComponent(envioMensagem);
+        
+        window.open("https://wa.me/+5579998005016?text="+ envioMensagem);
+    }
+}
